95dc904f8fc327b0eb910cb495ef13735ffef168
SamuelDAlencar <sarl.2001@hotmail.com>
2022-07-12 21:29:31 -0300

    chore

--- app/backend/src/routes/team/index.ts
+++ app/backend/src/routes/team/index.ts
@@ -14,6 +14,7 @@ const entityFactory = () => {
 const router = express.Router();
 
 router.get('/', (req, res) => entityFactory().getTeams(req, res));
+
 router.get('/:id', (req, res) => entityFactory().getTeamById(req, res));
 
 export default router;
